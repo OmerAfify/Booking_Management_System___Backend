a39aad6ab5857ddac33a692186b7ca8c89869fa9
OmerAfify <omarafify3102001@gmail.com>
2023-04-13 15:21:47 +0200

    Custom API responses for errors ard responses are handled.
    Reason is to return for the client a static default structure of errors and responses for every API call.

---
+++ BookingManagementSystem/Errors/ApiResponse.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookingManagementSystem.Errors
+{
+    public class ApiResponse
+    {
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+
+
+        public ApiResponse(int statusCode, string message = null)
+        {
+            StatusCode = statusCode;
+            Message = message ?? GetDefaultStatusCodeMessage(statusCode);
+
+        }
+
+        private static string GetDefaultStatusCodeMessage(int statusCode)
+        {
+            return statusCode switch
+            {
+                400 => "A bad request is sent.",
+                401 => "You are unauthorized.",
+                404 => "404 Not found.",
+                500 => "Internal Server Error.",
+                _ => null,
+            };
+        }
+    }
+}
