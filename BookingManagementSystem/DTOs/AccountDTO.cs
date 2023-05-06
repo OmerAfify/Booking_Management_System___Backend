e9db0c6e9062234f5d0fecd5cf3da40111dffb53
OmerAfify <omarafify3102001@gmail.com>
2023-05-06 22:07:35 +0300

    Validation added to password when adding a new user

--- BookingManagementSystem/DTOs/AccountDTO.cs
+++ BookingManagementSystem/DTOs/AccountDTO.cs
@@ -39,6 +39,8 @@ namespace BookingManagementSystem.DTOs
 
     public class AddNewUserDTO : GeneralUserDTO
     {
+        [Required]
+        [RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$", ErrorMessage = "Password must have Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character.")]
         public string Password { get; set; }
     }
 
